cfa965dfd57e623623f9bd5723f512c90ad61a84
bsspirit <bsspirit@163.com>
2012-11-01 12:06:20 +0800

    weibo valid

--- competition-php/protected/views/layouts/main.php
+++ competition-php/protected/views/layouts/main.php
@@ -3,7 +3,8 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-
+	<meta property="wb:webmaster" content="793452a43db7167c" />
+	
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/screen.css" media="screen, projection" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/print.css" media="print" />
